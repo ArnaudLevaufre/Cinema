8bfdc723249ae0320b8be34cc7d7408f1946447c
Arnaud Levaufre <arnaud@bananium.fr>
2016-01-03 16:24:17 +0100

    Add plot and download link

--- application/models.py
+++ application/models.py
@@ -4,15 +4,15 @@ from django.conf import settings
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=100)
     path = models.CharField(max_length=1024, unique=True)
+    title = models.CharField(max_length=100)
+    plot = models.TextField(null=True, blank=True)
     date = models.DateField(null=True, blank=True)
     imdbid = models.CharField(max_length=9, null=True, blank=True)
     poster = models.ImageField(upload_to="posters", null=True, blank=True)
 
     def path_to_static(self):
         return os.path.join(settings.MEDIA_URL, 'films', os.path.basename(self.path))
-
 
     class Meta:
         ordering = ['title']
