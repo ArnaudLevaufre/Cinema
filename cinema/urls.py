a476bffc519ed6f8dc53b7771b50d57052b9a5d6
Arnaud Levaufre <arnaud@bananium.fr>
2016-01-19 12:29:06 +0100

    Do not display «explore movies» link when authentication is required

--- cinema/urls.py
+++ cinema/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib import admin
 
 urlpatterns = [
-    url('login/', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url('login/', auth_views.login, {'template_name': 'login.html', 'extra_context': {'login_required': settings.LOGIN_REQUIRED}}, name='login'),
     url('logout/', auth_views.logout, {'next_page': '/login/'}, name='logout'),
     url('^', include('application.urls')),
     url('^admin/', admin.site.urls),
